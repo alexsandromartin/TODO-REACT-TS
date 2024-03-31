3dc575a7cadaafa7245edc50d9a5fde56ed76a06
Alexsandro Martins Alves <alexsandromartins.0172@gmail.com>
2024-03-31 13:27:36 -0300

    estilizando o header e o footer

---
+++ todo_list/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+//Css
+import styles from "./App.module.css"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className= {styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
+      <Footer/>
+  </div>
+  )
+}
+
+export default App;
